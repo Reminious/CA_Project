29b91cd2e386f256700371aa0e41e50cd8cf666e
ScofieldKKK010611 <147781338+ScofieldKKK010611@users.noreply.github.com>
2023-10-27 12:38:17 +0800

    new
    new

--- CA_Project/Data/Data.cs
+++ CA_Project/Data/Data.cs
@@ -3,6 +3,6 @@
     public class Data
     {
         //public static string CONNECTION_STRING = @"Server=DESKTOP-A2A7PBQ;Database=ShopDB;Integrated Security=true;encrypt=false";
-        public static string CONNECTION_STRING = @"Data Source=LAPTOP-OGVNJMDR;Initial Catalog=ShopDB;User ID=sa;Password=123456;TrustServerCertificate=true;";
+        public static string CONNECTION_STRING = @"Data Source=LAPTOP-OGVNJMDR;Initial Catalog=ShopDB;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
     }
 }
